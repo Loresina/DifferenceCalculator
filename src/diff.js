df528fc3db5b729322ad8a19b5eb9a6ecfa634a5
Ekaterina Volynshchikova <loresina86@gmail.com>
2023-06-25 03:34:53 +0700

    fix parser

--- src/diff.js
+++ src/diff.js
@@ -1,5 +1,6 @@
 import { cwd } from 'node:process';
 import path from 'path';
+import fs from 'fs';
 import compareObjects from './compare.js';
 import getObjectFromPath from './parser.js';
 import format from './formatters/index.js';
@@ -10,11 +11,17 @@ const getFullPath = (filePath) => {
   return fullPath;
 };
 
+const getFileFromPath = (fullPath) => {
+  const fileFromPuth = fs.readFileSync(fullPath);
+  const fileExtension = path.extname(fullPath).slice(1);
+  return getObjectFromPath(fileFromPuth, fileExtension);
+};
+
 const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
   const fullPath1 = getFullPath(filePath1);
   const fullPath2 = getFullPath(filePath2);
-  const fileObject1 = getObjectFromPath(fullPath1);
-  const fileObject2 = getObjectFromPath(fullPath2);
+  const fileObject1 = getFileFromPath(fullPath1);
+  const fileObject2 = getFileFromPath(fullPath2);
 
   const comparedObjects = compareObjects(fileObject1, fileObject2);
 
